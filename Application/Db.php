2ee130233e32de14309e60117f179caf549d3072
thernoff <thernoff@yandex.ru>
2016-12-01 11:08:45 +0400

    Добавлен метод query() в класс Db

--- Application/Db.php
+++ Application/Db.php
@@ -9,11 +9,25 @@ class Db
 	{
 		$this->dbh = new \PDO('mysql:host=localhost;dbname=test','root','');
 	}
-	
+	/*
+	 * Метод execute() используем для выполнения запросов без возвращения данных
+	 */
 	public function execute($sql)
 	{
 		$sth = $this->dbh->prepare($sql);
 		$res = $sth->execute();
 		return $res;
+	}
+	/*
+	 * Метод query() используем для выполнения запросов с возвращением данных
+	 */
+	public function query($sql)
+	{
+		$sth = $this->dbh->prepare($sql);
+		$res = $sth->execute();
+		if (false !== $res){
+			return $sth->fetchAll();
+		}
+		return [];
 	}
 }
--- index.php
+++ index.php
@@ -2,3 +2,5 @@
 require __DIR__ . '/autoload.php';
 
 $db = new \Application\Db();
+$arrRes = $db->query('SELECT * FROM news');
+var_dump($arrRes);
