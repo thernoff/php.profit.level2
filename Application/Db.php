7150242c4bb020a864581e6ca0790b62dec422c1
thernoff <thernoff@yandex.ru>
2016-12-01 10:56:48 +0400

    Добавлены метод execute() класса Db

--- Application/Db.php
+++ Application/Db.php
@@ -3,8 +3,17 @@ namespace Application;
 
 class Db
 {
+	protected $dbh;
+	
 	public function __construct()
 	{
-		echo 'Hello Db!';
+		$this->dbh = new \PDO('mysql:host=localhost;dbname=test','root','');
+	}
+	
+	public function execute($sql)
+	{
+		$sth = $this->dbh->prepare($sql);
+		$res = $sth->execute();
+		return $res;
 	}
 }
