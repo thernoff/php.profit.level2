8423077c9a6ffb0a30a8a9b50f7a944d5ac5373b
thernoff <thernoff@yandex.ru>
2016-12-03 12:34:42 +0400

    Реализован метод insert() класса Model

--- Application/Model.php
+++ Application/Model.php
@@ -8,6 +8,8 @@ abstract class Model
 {
 	const TABLE = '';
 	
+	public $id;//Принимаем соглашение: в каждой таблицы БД есть поле id с первичным ключом
+		
 	public static function findAll()
 	{
 		$db = Db::instance();
@@ -24,5 +26,38 @@ abstract class Model
 		}else{
 			return false;
 		}
+	}
+	
+	public function isNew()
+	{
+		return empty($this->id);
+	}
+	
+	
+	public function insert()
+	{
+		if (!$this->isNew())
+		{
+			return;//Если объект не является только что созданным, то выходим из метода insert()
+		}
+		
+		$columns = [];
+		$values = [];
+		foreach ($this as $key => $value){
+			if ('id' == $key){
+				continue;
+			}
+			
+			$columns[] = $key;
+			$values[':'.$key] = $value;
+		}
+		
+		//var_dump($columns);
+		
+		$sql = 'INSERT INTO ' . static::TABLE . ' ('. implode(',', $columns) .') VALUE (' . implode(',', array_keys($values)) . ')';
+		//echo $sql;
+		$db = Db::instance();
+		$db->execute($sql, $values);
+		
 	}
 }
--- index.php
+++ index.php
@@ -1,9 +1,14 @@
 <?php
-//use Application\Models\User;
+
 use Application\Models\News;
+use Application\Models\User;
 
 require __DIR__ . '/autoload.php';
 
-$news = News::getLastNews(3);
-include __DIR__ . '/Application/Views/news/index.php';
+//$news = News::getLastNews(3);
+//include __DIR__ . '/Application/Views/news/index.php';
+$user = new User();
+$user->name = "John";
+$user->email = "j@mail.com";
+$user->insert();
 //var_dump($users);
